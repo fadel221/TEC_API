4be89596825199e0f2fb97e5c3c6d4d5bb7d4dd1
fadel221 <gueyefadel22@gmail.com>
2022-07-04 05:31:23 +0000

    first deployment on heroku 8

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Utilisateur;
-use App\Services\UserService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,31 +10,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UtilisateurController extends AbstractController
 {
-    // #[Route('/utilisateur', name: 'app_utilisateur')]
-    // public function index(): Response
-    // {
-    //     return $this->render('utilisateur/index.html.twig', [
-    //         'controller_name' => 'UtilisateurController',
-    //     ]);
-    // }
+     #[Route('/utilisateur', name: 'app_utilisateur')]
+     public function index(): Response
+     {
+         return $this->render('utilisateur/index.html.twig', [
+             'controller_name' => 'UtilisateurController',
+         ]);
+     }
 
-    // /**
-    //  * @Route(
-    //  *     path="/api/admin/user}",
-    //  *     methods={"POST"},
-    //  *     defaults={
-    //  *          "__controller"="App\Controller\UtilisateurController::AddUser",
-    //  *          "__api_resource_class"=Utilisateur::class,
-    //  *          "__api_collection_operation_name"="add_user",
-    //  *         
-    //  *     }
-    //  * )
-    // */
-    // public function AddUser (UserService $user_service)
-    // {
-    //     dd();
-    //     $user=$user_service->AddUser();   
-    //     return $this->json($user,Response::HTTP_CREATED);
+     /**
+      * @Route(
+      *     path="/api/admin/user/{id}/image",
+      *     methods={"POST"},
+      *     defaults={
+      *          "__controller"="App\Controller\UtilisateurController::AddUser",
+      *          "__api_resource_class"=Utilisateur::class,
+      *          "__api_collection_operation_name"="add_user",
+      *         
+      *     }
+      * )
+     */
+//      public function updateUserImage ($id,UserRepository $rep )
+//      {
+//          dd();
+//          $user=$user_se;   
+//          return $this->json($user,Response::HTTP_CREATED);
                     
-    // } 
+//      } 
+// }
 }
